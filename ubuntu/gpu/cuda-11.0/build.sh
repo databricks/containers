74b1ceea258730b136387ea0e079123cc892b08f
Yu Gong <yu.gong@databricks.com>
2023-07-26 09:17:23 -0700

    update the tagged versions

--- ubuntu/gpu/cuda-11.0/build.sh
+++ ubuntu/gpu/cuda-11.0/build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -t databricksruntime/gpu-base:cuda11 base/
-docker build -t databricksruntime/gpu-conda:cuda11 conda/
-docker build -t databricksruntime/gpu-tensorflow:cuda11 tensorflow/
-docker build -t databricksruntime/gpu-pytorch:cuda11 pytorch/
-docker build -t databricksruntime/gpu-rapids:cuda11 rapids/
+docker build -t databricksruntime/gpu-base:cuda11.0 base/
+docker build -t databricksruntime/gpu-conda:cuda11.0 conda/
+docker build -t databricksruntime/gpu-tensorflow:cuda11.0 tensorflow/
+docker build -t databricksruntime/gpu-pytorch:cuda11.0 pytorch/
+docker build -t databricksruntime/gpu-rapids:cuda11.0 rapids/

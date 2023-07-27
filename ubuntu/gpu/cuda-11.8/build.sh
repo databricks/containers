d6477c792d8c31916c34d3ba5460c62573a911e3
Yu Gong <yu.gong@databricks.com>
2023-07-26 18:59:13 -0700

    update the environment

--- ubuntu/gpu/cuda-11.8/build.sh
+++ ubuntu/gpu/cuda-11.8/build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -t databricksruntime/gpu-base:cuda11 base/
-docker build -t databricksruntime/gpu-conda:cuda11 conda/
+docker build -t databricksruntime/gpu-base:cuda11.8 base/
+docker build -t databricksruntime/gpu-conda:cuda11.8 conda/
 docker build -t databricksruntime/gpu-tensorflow:cuda11 tensorflow/
 docker build -t databricksruntime/gpu-pytorch:cuda11 pytorch/
 docker build -t databricksruntime/gpu-rapids:cuda11 rapids/
